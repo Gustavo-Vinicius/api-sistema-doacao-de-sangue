c607ad355b9a9b8d5bde03c2a7bd7b8ac609cff1
Gustavo-Vinicius <gustavovinicimos12345@gmail.com>
2024-05-22 15:08:02 -0300

    feat : Adicionando rotina de buscar doador por Id.

--- Sistema-de-Doacao-de-Sangue.Application/Queries/DoadoresQueries/ObterDoadorPorId/ObterDoadorPorIdQueryHandler.cs
+++ Sistema-de-Doacao-de-Sangue.Application/Queries/DoadoresQueries/ObterDoadorPorId/ObterDoadorPorIdQueryHandler.cs
@@ -1,10 +1,19 @@
 using MediatR;
 using Sistema_de_Doacao_de_Sangue.Core.DTOs;
+using Sistema_de_Doacao_de_Sangue.Core.Repositories;
 
 namespace Sistema_de_Doacao_de_Sangue.Application.Queries.DoadoresQueries.ObterDoadorPorId
 {
-    public class ObterDoadorPorIdQueryHandler : IRequest<DoadorDTO>
+    public class ObterDoadorPorIdQueryHandler : IRequestHandler<ObterDoadorPorIdQuery, DoadorDTO>
     {
-        public int Id { get; set; }
+        private readonly IDoadoresRepository _doadoresRepository;
+        public ObterDoadorPorIdQueryHandler(IDoadoresRepository doadoresRepository)
+        {
+            _doadoresRepository = doadoresRepository;
+        }
+        public async Task<DoadorDTO> Handle(ObterDoadorPorIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _doadoresRepository.ObterDoadorPorId(request.Id);
+        }
     }
 }
